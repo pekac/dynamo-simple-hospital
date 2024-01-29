ea4cce387811d03c44de56f7e14d41dc37528f12
Predrag Popovic <pedja.popovic9@gmail.com>
2024-01-29 21:35:40 +0100

    Add helper method for decapitalizing text

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,3 +1,7 @@
 export function capitalize(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
+
+export function decapitalize(text: string): string {
+  return text.charAt(0).toLowerCase() + text.slice(1);
+}
