7bb6c155576c2d83887d92eb417e236d5d34ecb0
Predrag Popovic <pedja.popovic9@gmail.com>
2024-02-07 17:34:31 +0100

    Add assign-patient use case

---
+++ src/doctors/use-cases/assign-patient-to-doctor.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Module,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  CommandBus,
+  CommandHandler,
+  CqrsModule,
+  ICommandHandler,
+} from '@nestjs/cqrs';
+
+import { IDoctorsService } from '../doctor.interface';
+import { DoctorsService } from '../doctors.service';
+import { AssignPatientToDoctorDto } from '../doctor.dto';
+
+class AssignPatientToDoctorCommand {
+  constructor(
+    public readonly doctorId: string,
+    public readonly assignPatientDto: AssignPatientToDoctorDto,
+  ) {}
+}
+
+@Controller()
+class AssignPatientToDoctorController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @Post('doctors/:id/patients')
+  @UsePipes(new ValidationPipe())
+  addPatientToDoctor(
+    @Param('id') doctorId: string,
+    @Body() assignPatientDto: AssignPatientToDoctorDto,
+  ) {
+    return this.commandBus.execute(
+      new AssignPatientToDoctorCommand(doctorId, assignPatientDto),
+    );
+  }
+}
+
+@CommandHandler(AssignPatientToDoctorCommand)
+class AssignPatientToDoctorHandler
+  implements ICommandHandler<AssignPatientToDoctorCommand>
+{
+  constructor(private readonly doctorsService: IDoctorsService) {}
+
+  async execute({ doctorId, assignPatientDto }: AssignPatientToDoctorCommand) {
+    return this.doctorsService.addPatient(doctorId, assignPatientDto);
+  }
+}
+
+@Module({
+  imports: [CqrsModule],
+  controllers: [AssignPatientToDoctorController],
+  providers: [
+    AssignPatientToDoctorHandler,
+    { provide: IDoctorsService, useClass: DoctorsService },
+  ],
+})
+export class AssignPatientToDoctorModule {}
