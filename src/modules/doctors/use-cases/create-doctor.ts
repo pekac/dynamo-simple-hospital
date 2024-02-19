8cfc08960853dff78a02ff6aabba815e271973c0
Predrag Popovic <pedja.popovic9@gmail.com>
2024-02-19 10:08:10 +0100

    Update create doctor usecase with item actions

--- src/modules/doctors/use-cases/create-doctor.ts
+++ src/modules/doctors/use-cases/create-doctor.ts
@@ -13,12 +13,40 @@ import {
   ICommandHandler,
 } from '@nestjs/cqrs';
 
+import { ITEM_BASED_ACTIONS, ItemKey, itemActionGenerator } from 'src/dynamo';
+
 import { CreateDoctorDto } from '../doctor.dto';
 
 import { DoctorAlreadyExistsException } from '../doctor.exceptions';
+import { DOCTOR_ID_PREFIX, Doctor } from 'src/core';
+
+interface IDoctorActions {
+  create(
+    createDto: Partial<Doctor>,
+    parentId?: string,
+  ): Promise<string | undefined>;
+  one: (doctorId: string) => Promise<Doctor | undefined>;
+}
 
 class CreateDoctorCommand {
   constructor(public readonly createDoctorDto: CreateDoctorDto) {}
+}
+
+function decorateDoctor(doctor: CreateDoctorDto & ItemKey) {
+  const specialization = doctor.specialization.toUpperCase();
+  return {
+    ...doctor,
+    /* for fetching tests */
+    GSI1PK: doctor.PK,
+    GSI1SK: doctor.SK,
+    /* for listing by specialization */
+    specialization,
+    GSI2PK: `SPECIALIZATION#${specialization}`,
+    GSI2SK: `${specialization}#${doctor.id}`,
+    /* for listing patients */
+    GSI3PK: doctor.PK,
+    GSI3SK: doctor.SK,
+  };
 }
 
 @Controller()
@@ -34,18 +62,19 @@ class CreateDoctorController {
 
 @CommandHandler(CreateDoctorCommand)
 class CreateDoctorHandler implements ICommandHandler<CreateDoctorCommand> {
-  constructor() {}
+  private readonly itemActions = itemActionGenerator({
+    entityTemplate: Doctor,
+    actions: [ITEM_BASED_ACTIONS.CREATE, ITEM_BASED_ACTIONS.GET],
+    pkPrefix: DOCTOR_ID_PREFIX,
+    decorate: decorateDoctor,
+  }) as IDoctorActions;
 
   async execute({ createDoctorDto }: CreateDoctorCommand) {
-    try {
-      // const doctor = await this.doctorsService.one(createDoctorDto.id);
-      // if (doctor) {
-      //   throw new DoctorAlreadyExistsException(createDoctorDto.id);
-      // }
-      // return this.doctorsService.create(createDoctorDto);
-    } catch (e) {
-      throw new Error(e.message);
+    const doctor = await this.itemActions.one(createDoctorDto.id);
+    if (doctor) {
+      throw new DoctorAlreadyExistsException(createDoctorDto.id);
     }
+    return this.itemActions.create(createDoctorDto);
   }
 }
 
