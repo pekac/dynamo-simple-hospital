2527bdde2013a92a1f6df0968deaa2baf7416c58
Predrag Popovic <pedja.popovic9@gmail.com>
2024-02-22 14:24:50 +0100

    Update doctor resource

--- src/dynamo/data/doctors.resource.ts
+++ src/dynamo/data/doctors.resource.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
-import { DOCTOR_ID_PREFIX, Doctor } from 'src/core';
+import { CreateDoctorDto, DOCTOR_ID_PREFIX, Doctor } from 'src/core';
 
-import { Resource } from '../resource';
+import { CreateItem, ItemKey, Resource, isCreateItem } from '../resource';
 
 @Injectable()
 export class DoctorsResource extends Resource<Doctor> {
   constructor() {
     super({ entityTemplate: Doctor, pkPrefix: DOCTOR_ID_PREFIX });
   }
+
+  create(
+    dto: CreateDoctorDto | CreateItem<Doctor>,
+  ): Promise<string | undefined> {
+    if (isCreateItem(dto)) {
+      throw new TypeError(`Dto is not of shape CreateDoctorDto`);
+    }
+
+    return super.create({
+      dto,
+      decorator: decorateDoctor,
+    });
+  }
+}
+
+function decorateDoctor(doctor: CreateDoctorDto & ItemKey) {
+  const specialization = doctor.specialization.toUpperCase();
+  return {
+    ...doctor,
+    /* for fetching tests */
+    GSI1PK: doctor.PK,
+    GSI1SK: doctor.SK,
+    /* for listing by specialization */
+    specialization,
+    GSI2PK: `SPECIALIZATION#${specialization}`,
+    GSI2SK: `${specialization}#${doctor.id}`,
+    /* for listing patients */
+    GSI3PK: doctor.PK,
+    GSI3SK: doctor.SK,
+  };
 }
--- src/dynamo/index.ts
+++ src/dynamo/index.ts
@@ -1,2 +1,3 @@
+export * from './client';
 export * from './dynamo.module';
 export * from './migration-controller';
