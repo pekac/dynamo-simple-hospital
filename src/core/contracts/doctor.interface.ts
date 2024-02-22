50e803cf730ea93f56f93f267410321037c15ee5
Predrag Popovic <pedja.popovic9@gmail.com>
2024-02-22 14:19:18 +0100

    Extract doctor dtos to core

--- src/core/contracts/doctor.interface.ts
+++ src/core/contracts/doctor.interface.ts
@@ -1,29 +1,33 @@
-import { Doctor, Patient } from '../entities';
+import { CreateDoctorDto } from '../dto';
+
+import { Doctor } from '../entities';
 
 export abstract class IDoctorsResource {
-  abstract create(createDoctorDto: Doctor): Promise<Doctor | undefined>;
+  abstract create(
+    createDoctorDto: CreateDoctorDto,
+  ): Promise<Doctor | undefined>;
   abstract one(doctorId: string): Promise<Doctor | undefined>;
   abstract update(
     doctorId: string,
     updateDoctorDto: Partial<Doctor>,
   ): Promise<Doctor>;
   abstract remove(doctorId: string): Promise<string>;
-  abstract list(
-    startCollection: string,
-    limit: number,
-    lastSeen: string,
-  ): Promise<Doctor[]>;
-  abstract addPatient(
-    doctorId: string,
-    addPatientDto: Partial<Patient>,
-  ): Promise<any>;
-  abstract removePatientFromDoctor(
-    doctorId: string,
-    patientId: string,
-  ): Promise<any>;
-  abstract listDoctorsForPatient(
-    patientId: string,
-    limit?: number,
-    lastSeen?: string,
-  ): Promise<Doctor[]>;
+  // abstract list(
+  //   startCollection: string,
+  //   limit: number,
+  //   lastSeen: string,
+  // ): Promise<Doctor[]>;
+  // abstract addPatient(
+  //   doctorId: string,
+  //   addPatientDto: Partial<AssignPatientToDoctorDto>,
+  // ): Promise<any>;
+  // abstract removePatientFromDoctor(
+  //   doctorId: string,
+  //   patientId: string,
+  // ): Promise<any>;
+  // abstract listDoctorsForPatient(
+  //   patientId: string,
+  //   limit?: number,
+  //   lastSeen?: string,
+  // ): Promise<Doctor[]>;
 }
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 export * from './constants';
 export * from './contracts';
+export * from './dto';
 export * from './entities';
